b379fbcb6507d82286a3d88ae3b2f404251cb2b7
Rushing <liming20110711@163.com>
2018-07-23 08:07:16 +0800

    update:20180723-01

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -79,6 +79,19 @@ create table `seller_info` (
 	primary key (`seller_id`)
 ) comment '卖家信息表';
 
+drop table if exists user_info;
+CREATE TABLE `user_info` (
+  `id` varchar(32) NOT NULL,
+  `username` varchar(32) DEFAULT '',
+  `password` varchar(32) DEFAULT '',
+  `openid` varchar(64) DEFAULT '' COMMENT '微信openid',
+  `role` tinyint(1) NOT NULL COMMENT '1买家2卖家',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`id`)
+) comment '用户表';
+
+
 
 
 
